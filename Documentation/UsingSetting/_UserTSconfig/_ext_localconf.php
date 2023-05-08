5341b7a9d4f4e5e1b41d87be1673fee2b3ac3256
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2023-05-08 17:17:46 +0200

    Update Documentation/UsingSetting/_UserTSconfig/_ext_localconf.php
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

--- Documentation/UsingSetting/_UserTSconfig/_ext_localconf.php
+++ Documentation/UsingSetting/_UserTSconfig/_ext_localconf.php
@@ -7,7 +7,7 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 defined('TYPO3') or die();
 
 /**
- * Adding the default User TSconfig
+ * Adding the default user TSconfig
  */
 ExtensionManagementUtility::addUserTSConfig(
     '@import "EXT:my_sitepackage/Configuration/TsConfig/User/default.tsconfig"'
